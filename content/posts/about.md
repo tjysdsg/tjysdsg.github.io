3c7998af464a94faf406297b6c5f7fd574214d0c
tjysdsg <tjy1018543509@163.com>
2021-05-29 12:11:29 +0800

    Clear

--- content/posts/about.md
+++
@@ -1,6 +0,0 @@
----
-title: "About"
-date: 2021-05-29T10:49:30+08:00
-draft: true
----
-
--- push-to-gh-pages-branch.sh
+++
@@ -1,2 +0,0 @@
-# https://stackoverflow.com/a/36782614/7730917
-git subtree push --prefix public origin gh-pages
