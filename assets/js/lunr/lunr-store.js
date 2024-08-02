6304f3180de1464ef51faf5776376342ffc258d8
tjysdsg <tjysdsg@users.noreply.github.com>
2024-08-02 06:30:44 +0000

    jekyll build from Action 21150b03f4acbfe2b25fb0b75fef0cb226cb8543

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Adding mathjax to jekyll",
+        "excerpt":"Steps 1. Check for _includes/head.html file in your project If you don’t have _includes/head.html in your jekyll project, create one. Be careful that it will override the _includes/head.html from the theme, so you should copy the content of it from the original theme files. For example, I’m using minimal mistakes...","categories": [],
+        "tags": [],
+        "url": "/mathjax-jekyll",
+        "teaser": null
+      },{
+        "title": "Understanding the Math Behind CTC",
+        "excerpt":"In this post, I’m writing down my thought process of understanding the math behind Connectionist Temporal Classification (CTC) [1][2]. This article assumes that you understand the intuition behind CTC. Definitions Input and Output Sequences The input of a CTC network (an audio signal) is a sequence denoted as \\(\\boldsymbol{x} =...","categories": [],
+        "tags": [],
+        "url": "/ctc",
+        "teaser": null
+      },{
+        "title": "C/C++: printing stacktrace containing file name, function name, and line numbers using libbacktrace",
+        "excerpt":"I needed a library for printing stack traces when developing tan. More specifically, the compiler and the runtime had to print the function names, source file names, and the line numbers when an assertion failed or an error was raised. Initial Solution The library was initially implemented using libunwind. The...","categories": [],
+        "tags": [],
+        "url": "/libbacktrace",
+        "teaser": null
+      },{
+        "title": "Machine Learning Notes",
+        "excerpt":"Machine Learning review notes Fundamentals Overfitting/Underfitting Overfitting = a model fits too well against its training data, and it cannot perform accurately against unseen data, thus has high generalization error/out-of-sample error/risk Underfitting = a data model is unable to capture the relationship between the input and output variables accurately, generating...","categories": [],
+        "tags": [],
+        "url": "/ml-notes",
+        "teaser": null
+      }]
